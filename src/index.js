c32936b39093dc7431d0736f6fdc547dd3725daf
Ayush Gupta <ayushgupta3902@gmail.com>
2023-12-10 22:59:02 +0530

    feat: add models; setup ports and utils

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import { PORT } from "./constants.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-const app = express();
+connectDB()
+  .then(() => {
+    app.listen(PORT || 8000, () => {
+      `Server started on port ${PORT}`
+    })
+  })
+  .catch((error) => console.log("MONGODB connection failed !!", error));
 
-connectDB();
 
-app.listen(`${process.env.PORT}`, () =>
-  console.log(`Server started on port ${process.env.PORT}`)
-);
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//  *** Alternative Method to do the above ***
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
